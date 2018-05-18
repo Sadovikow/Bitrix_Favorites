049b98da7c6737d4effcd7f37f108ec7d58ee541
Sergey Sadovikov <nexxerr@gmail.com>
2018-05-18 16:15:00 +0300

    Checking empty IDs

--- favoriteButton.php
+++ favoriteButton.php
@@ -2,16 +2,31 @@
  /* Вывод количества избранного */
     if(!$USER->IsAuthorized()) // Для неавторизованного
     {
-            $arElements = unserialize($APPLICATION->get_cookie('favorites'));
-            $wishCount = count($arElements);
-            if($arElements == '') 
-               unset($arElements);
+        $arElements = unserialize($APPLICATION->get_cookie('favorites'));
+        if($arElements == '')
+								 	unset($arElements);
+
+								foreach($arElements as $k=>$fav) // Checking empty IDs
+								{
+									if($fav == '0')
+										 unset($arElements[$k]);
+									 	unset($fav);
+								}
+        $wishCount = count($arElements);
     }
     else {
          $idUser = $USER->GetID();
          $rsUser = CUser::GetByID($idUser);
          $arUser = $rsUser->Fetch();
-         $wishCount = count($arUser['UF_FAVORITES']);
+        foreach($arUser['UF_FAVORITES'] as $k=>$fav) // Checking empty IDs
+								{
+									if($fav == '0')
+										unset($arUser['UF_FAVORITES'][$k]);
+										unset($fav);
+								}
+        $wishCount = count($arUser['UF_FAVORITES']);
+
+
      }
  /* Вывод количества избранного */
 ?>
